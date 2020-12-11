4d21f04fd1ae3059a3b5db62e11bb43fba1338c6
abdo shaban <abdoshaban@safavisa.com>
2020-12-11 18:41:33 +0200

    user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,16 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'first_name'        => $this->faker->name,
+            'last_name'         => $this->faker->name,
+            'country_code'      => $this->faker->countryCode,
+            'phone_number'      => $this->faker->e164PhoneNumber,
+            'gender'            => $this->faker->randomElement(['male', 'female']),
+            'birthdate'         => $this->faker->date(),
+            'email'             => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'api_token'         => hash('sha256', Str::random(60)),
         ];
     }
 }
